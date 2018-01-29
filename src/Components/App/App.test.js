863569b2a4b36dd10cff892610ae58ec46308a82
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-01-29 10:11:37 -0700

    Set up initial tests for App component

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  })
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it('shold have a class name of .App', () => {
+    expect(wrapper.find('div').hasClass('App')).toEqual(true);
+  });
+
 });
+
