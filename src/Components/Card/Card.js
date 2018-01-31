64be32eda60f2d298221c12d70025dc233b71631
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-01-31 16:05:25 -0700

    Break out different html structure for different type of cards and edit the way info is passed into Card component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ name, homeworld, species, population }) => {
+const Card = ({ type, item }) => {
+  const details = { ...item } 
+  let { name, population } = details
+  let { homeworld, species } = details
+  let { terrain, climate, residents } = details
+
+  const peopleDetails =  
+    <div>
+      <ul>
+        <li>Homeworld: {homeworld}</li>
+        <li>Species: {species}</li>
+        <li>Population: {population}</li>
+      </ul>
+    </div>
+
+  const planetDetails = 
+    <div>
+      <ul>
+        <li>Terrain: {terrain}</li>
+        <li>Climate: {climate}</li>
+        <li>Population: {population}</li>
+        <li>Residents: {residents}</li>
+      </ul>
+    </div>
+
+  const cardRendered = () => {
+    if(type === 'people') {
+      return peopleDetails;
+    } else if(type === 'planets') {
+      return planetDetails;
+    }
+  }
 
   return (
     <div>
       <h2>{ name }</h2>
-      <ul>
-        <li>{ homeworld }</li>
-        <li>{ species }</li>
-        <li>{ population }</li>
-      </ul>
+      { cardRendered() }  
       <button>+</button>
     </div>
   )
--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Card from '../Card/Card';
 import './CardContainer.css';
 
-const CardContainer = ({ arrayToDisplay }) => {
-  const renderedCards = arrayToDisplay.map( item => <Card { ...item } />)
+const CardContainer = ({ arrayToDisplay, type }) => {
+  const renderedCards = arrayToDisplay.map( item => <Card item={ item } type={ type }/>)
 
   return (
     <div>
