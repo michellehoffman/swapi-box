fd79cef70f9c5354955b0793f88ff1c8c30c3cb9
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-01-30 21:00:17 -0700

    Make Controls component stateful and update className on rerender

--- src/Components/Controls/Controls.js
+++ src/Components/Controls/Controls.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Controls.css';
 
-const Controls = ({ favorites, dataToDisplay }) => {
-  const getElement = e => {
-    const element = e.target.innerText;
-    dataToDisplay(element);
+class Controls extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: null
+    }
   }
 
-  const num = favorites ? favorites.length : 0
+  getElement = e => {
+    const value = e.target.innerText;
 
-  return (
-    <div>
+    this.setState({ active: value });
+    this.props.dataToDisplay(value);
+  }
+
+  setClass(buttonType) {
+    return buttonType === this.state.active ? 'active' : 'inactive';
+  }
+
+  num = () => {
+    return this.props.favorites ? this.props.favorites.length : 0;
+  } 
+
+  render() {
+    return (
       <div>
-        <button onClick={ getElement }>people</button>
-        <button onClick={ getElement }>planets</button>
-        <button onClick={ getElement }>vehicles</button>
-      </div>
-      <div>
-        <button>favorites <span>{ num }</span></button>
-      </div>
-    </div> 
-  )
+        <div>
+          <button className={ this.setClass('people') } 
+                  onClick={ this.getElement }>people</button>
+          <button className={ this.setClass('planets') }
+                  onClick={ this.getElement }>planets</button>
+          <button className={ this.setClass('vehicles') } 
+                  onClick={ this.getElement }>vehicles</button>
+        </div>
+        <div>
+          <button className={ this.setClass('favorites') }>favorites <span>{ this.num() }</span></button>
+        </div>
+      </div> 
+    )
+  }
 }
 
 export default Controls;
